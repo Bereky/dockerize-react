d1299b1d26b118277c52d0691f10e276bc747be8
unknown <bereketlemma4@gmail.com>
2023-09-16 12:04:30 +0300

    added animated docker icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import dockerLogo from './assets/docker.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -14,6 +16,9 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://docker.com" target="_blank">
+          <img src={dockerLogo} className="logo docker" alt="Docker logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
